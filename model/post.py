1aa0d807207239d9ae9cf43d8e99531f0f7daaf1
guzhiyiai <guzhiyiai@gmail.com>
2015-03-10 15:52:40 +0800

    model: add post model

--- model/post.py
+++ model/post.py
@@ -1,8 +1,18 @@
+# -*- coding: utf-8 -*-
 
-class Post(object):
-    def __init__(self, name):
-        self.name = name
+from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.ext.declarative import declarative_base
 
-    @staticmethod
-    def get(name):
-        return Post(name)
+# 创建对象的基类:
+Base = declarative_base()
+
+# 定义对象:
+class Post(Base):
+    __tablename__ = 'post'
+
+    id = Column(Integer, primary_key=True)
+    title = Column('title', String(50))
+    content = Column('content', Text, default='', nullable=False)
+
+
+
