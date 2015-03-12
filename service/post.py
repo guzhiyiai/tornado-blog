1050a0bf00930addfe10b5ee896685a2c73ecbfb
guzhiyiai <guzhiyiai@gmail.com>
2015-03-12 16:29:46 +0800

    service: add service

---
+++ service/post.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from models import Post
+from extensions import db
+
+
+class PostService(object):
+
+    @staticmethod
+    def add(title, content):
+        post = Post(title=title, content=content)
+        db.add(post)
+        db.commit()
+        return post.to_dict()
+
+    @staticmethod
+    def get_list():
+         posts = db.query(Post).all()
+         return [post.to_dict() for post in posts]
